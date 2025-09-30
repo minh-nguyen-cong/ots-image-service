2f5f230025c625fa24a0694e8cc717362b29118b
Minh Nguyen <91minhnguyen@gmail.com>
2025-09-30 12:18:03 +0700

    Revert "Enable send message for successful uploading"
    This reverts commit 9c989c77b4a18f35dab865db4c083a8711ade6fd.

--- src/main/java/com/cdc/ots_image_service/service/ImageService.java
+++ src/main/java/com/cdc/ots_image_service/service/ImageService.java
@@ -2,9 +2,6 @@ package com.cdc.ots_image_service.service;
 
 import com.cdc.ots_image_service.entity.Image;
 import com.cdc.ots_image_service.repository.ImageRepository;
-import com.google.cloud.eventarc.publishing.v1.EventarcPublisherClient;
-import com.google.cloud.eventarc.publishing.v1.PublishEventsRequest;
-import com.google.cloud.eventarc.publishing.v1.PublishEventsResponse;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
@@ -13,36 +10,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import com.google.protobuf.Any;
-import com.google.protobuf.ByteString;
-import io.cloudevents.v1.proto.CloudEvent.CloudEventAttributeValue;
-import io.cloudevents.v1.proto.CloudEvent.Builder;
 
 @Service
 public class ImageService {
 
     private final ImageRepository imageRepository;
     private final Storage storage;
-    private final EventarcPublisherClient eventarcPublisherClient;
-    private final String eventarcChannel;
 
     @Value("${gcs.bucket.raw-images}")
     private String rawImagesBucket;
 
-    public ImageService(
-            ImageRepository imageRepository,
-            Storage storage,
-            @Value("${gcp.eventarc.project-id}") String projectId,
-            @Value("${gcp.eventarc.location}") String location,
-            @Value("${gcp.eventarc.channel}") String channel) throws IOException {
+    public ImageService(ImageRepository imageRepository, Storage storage) {
         this.imageRepository = imageRepository;
         this.storage = storage;
-        this.eventarcPublisherClient = EventarcPublisherClient.create();
-        this.eventarcChannel = String.format("projects/%s/locations/%s/channels/%s", projectId, location, channel);
     }
 
     public Image uploadImage(MultipartFile file, String uploaderEmail) throws IOException {
@@ -67,23 +50,15 @@ public class ImageService {
         Image savedImage = imageRepository.save(image);
 
         // 4. Publish a CloudEvent to Eventarc to trigger the thumbnail worker
-        Builder eventBuilder = io.cloudevents.v1.proto.CloudEvent.newBuilder()
-                .setId(UUID.randomUUID().toString())
-                .setSource(URI.create("image-service").toString())
-                .setSpecVersion("1.0")
-                .setType("com.cdc.image.uploaded");
+        // CloudEvent event = CloudEventBuilder.v1()
+        //         .withId(UUID.randomUUID().toString())
+        //         .withSource(URI.create("image-service"))
+        //         .withType("com.cdc.ots.image.uploaded")
+        //         .withDataContentType("application/json")
+        //         .withData(savedImage.getId().toString().getBytes())
+        //         .build();
 
-        eventBuilder.putAttributes("datacontenttype",
-                CloudEventAttributeValue.newBuilder().setCeString("application/json").build());
-
-        eventBuilder.setProtoData(Any.newBuilder().setValue(ByteString.copyFromUtf8(savedImage.getId().toString())).build());
-
-        PublishEventsRequest request = PublishEventsRequest.newBuilder()
-                .setChannel(eventarcChannel)
-                .addEvents(eventBuilder.build())
-                .build();
-
-        PublishEventsResponse response = this.eventarcPublisherClient.publishEvents(request);
+        // eventarcMessageSender.send(event);
 
         return savedImage;
     }
