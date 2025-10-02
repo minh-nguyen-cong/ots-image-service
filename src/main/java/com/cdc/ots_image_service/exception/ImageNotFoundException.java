af844482df85c043e9044e161e77a685b7daf05c
Minh Nguyen <91minhnguyen@gmail.com>
2025-10-03 05:02:50 +0700

    Correct ImageNotFoundException constructor

--- src/main/java/com/cdc/ots_image_service/exception/ImageNotFoundException.java
+++ src/main/java/com/cdc/ots_image_service/exception/ImageNotFoundException.java
@@ -1,7 +1,14 @@
 package com.cdc.ots_image_service.exception;
 
 public class ImageNotFoundException extends RuntimeException {
-    public ImageNotFoundException(String message) {
+    private final Long id;
+
+    public ImageNotFoundException(String message, Long id) {
         super(message);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
