63574245b0c9888adcbed54509b3bf12045cc393
Minh Nguyen <91minhnguyen@gmail.com>
2025-10-04 16:45:08 +0700

    Correctness

--- src/main/java/com/cdc/ots_image_service/security/SecurityConfig.java
+++ src/main/java/com/cdc/ots_image_service/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cdc.ots_image_service.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
