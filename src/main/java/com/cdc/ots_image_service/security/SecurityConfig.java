e588bac66fe84e64e0e831954ae8a4c4df098ffd
Minh Nguyen <91minhnguyen@gmail.com>
2025-10-06 15:55:49 +0700

    Correctness

--- src/main/java/com/cdc/ots_image_service/security/SecurityConfig.java
+++ src/main/java/com/cdc/ots_image_service/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(allowedOriginPatterns));
+        configuration.setAllowedOriginPatterns(List.of(allowedOriginPatterns));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
